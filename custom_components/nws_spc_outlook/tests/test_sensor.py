532c991f855b5b6e19dd0914effeb185afec8db0
Steve Edwards <sedward5@emich.edu>
2024-11-10 05:40:55 +0000

    More lint

--- custom_components/nws_spc_outlook/tests/test_sensor.py
+++ custom_components/nws_spc_outlook/tests/test_sensor.py
@@ -8,15 +8,18 @@ from custom_components.nws_spc_outlook.sensor import (
 
 @pytest.fixture
 def coordinator(hass):
+    """Create coordinator for tests."""
     return NWSSPCOutlookDataCoordinator(hass, 35.0, -97.0)
 
 
-def test_sensor_initialization(coordinator):
+def test_sensor_initialization(coordinator) -> None:
+    """Test sensor initialization."""
     sensor = NWSSPCOutlookSensor(coordinator, 1)
     assert sensor.name == "SPC Outlook Day 1"
 
 
-def test_sensor_update(coordinator):
+def test_sensor_update(coordinator) -> None:
+    """Test sensor_update funtion."""
     sensor = NWSSPCOutlookSensor(coordinator, 1)
     sensor.update()
     assert sensor.state == "15%"
