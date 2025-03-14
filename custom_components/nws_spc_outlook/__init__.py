744ee8fab2f0f74942f373d7dbd9ebf21b408483
Steve Edwards <sedward5@emich.edu>
2025-03-13 21:38:05 -0400

    lint ficx

--- custom_components/nws_spc_outlook/__init__.py
+++ custom_components/nws_spc_outlook/__init__.py
@@ -1,10 +1,10 @@
 """Initialize the NWS SPC Outlook integration."""
 import logging
 
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
+from .const import DOMAIN  # noqa: F401
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -12,4 +12,9 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up nws_spc_outlook from a config entry."""
     _LOGGER.debug("Setting up NWS SPC Outlook integration entry")
+
+    # Mark arguments as used to avoid linter errors
+    _ = hass
+    _ = entry
+
     return True
