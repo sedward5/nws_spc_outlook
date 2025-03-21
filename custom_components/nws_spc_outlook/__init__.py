a831521bd4a3d3963bb0fb6b9e2eea0979c26348
Steve Edwards <sedward5@emich.edu>
2025-03-20 23:36:25 -0400

    Update __init__.py

--- custom_components/nws_spc_outlook/__init__.py
+++ custom_components/nws_spc_outlook/__init__.py
@@ -7,6 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN, CONF_LATITUDE, CONF_LONGITUDE
+from .coordinator import NWSSPCOutlookDataCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
