b8cf8892c30e5fe778b2fd2aefdfb2b3e7502697
Steve Edwards <sedward5@emich.edu>
2025-03-21 20:47:03 -0400

    Update const.py

--- custom_components/nws_spc_outlook/const.py
+++ custom_components/nws_spc_outlook/const.py
@@ -10,3 +10,6 @@ CONF_LONGITUDE = "longitude"
 DEFAULT_NAME = "NWS SPC Outlook"
 DEFAULT_LATITUDE = 42.2808  # Approximate latitude for Ann Arbor, MI
 DEFAULT_LONGITUDE = -83.7430  # Approximate longitude for Ann Arbor, MI
+
+BASE_URL = "https://www.spc.noaa.gov/products/outlook"
+DAYS_WITH_DETAILED_OUTLOOKS = 3
