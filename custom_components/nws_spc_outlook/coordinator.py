c45d5d15e62165d97ef61c139cad9650c9986042
Steve Edwards <sedward5@emich.edu>
2025-03-23 16:11:29 -0400

    Update coordinator.py

--- custom_components/nws_spc_outlook/coordinator.py
+++ custom_components/nws_spc_outlook/coordinator.py
@@ -1,8 +1,12 @@
-from datetime import timedelta
+"""Coordinator for fetching and updating SPC Outlook data."""
+
 import logging
+from datetime import timedelta
+
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
 from .api import getspcoutlook
 
 _LOGGER = logging.getLogger(__name__)
@@ -10,7 +14,7 @@ _LOGGER = logging.getLogger(__name__)
 class NWSSPCOutlookDataCoordinator(DataUpdateCoordinator):
     """Coordinator to fetch and update SPC Outlook data."""
 
-    def __init__(self, hass: HomeAssistant, latitude: float, longitude: float):
+    def __init__(self, hass: HomeAssistant, latitude: float, longitude: float) -> None:
         """Initialize the coordinator."""
         super().__init__(
             hass,
@@ -22,6 +26,6 @@ class NWSSPCOutlookDataCoordinator(DataUpdateCoordinator):
         self.longitude = longitude
         self.session = async_get_clientsession(hass)
 
-    async def _async_update_data(self):
+    async def _async_update_data(self) -> dict:
         """Fetch the latest SPC Outlook data."""
         return await getspcoutlook(self.latitude, self.longitude, self.session)
