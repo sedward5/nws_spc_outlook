c0388bcd162219d3a24253a3133df4602f4e541c
Guy Hale <guyhalestorm@gmail.com>
2026-03-06 23:35:18 -0600

    Update api.py to pull new CIG data

--- custom_components/nws_spc_outlook/api.py
+++ custom_components/nws_spc_outlook/api.py
@@ -118,12 +118,22 @@ async def getspcoutlook(
 
             polygon = shape(geometry)
             if polygon.contains(location):
-                output[key] = feature["properties"].get("LABEL2", "Unknown")
-                output[f"{key}_attributes"] = {
+                label = feature["properties"].get("LABEL", "")
+                label2 = feature["properties"].get("LABEL2", "Unknown")
+                
+                # Check if this is a standard risk or CIG
+                if str(label).startswith("CIG") or "Conditional" in label2:
+                    target_key = f"{key}_cig"
+                else:
+                    target_key = key
+                    
+                # Save the data to separate keys
+                output[target_key] = label2
+                output[f"{target_key}_attributes"] = {
                     "valid": feature["properties"].get("VALID", "Unknown"),
                     "issue": feature["properties"].get("ISSUE", "Unknown"),
                     "expire": feature["properties"].get("EXPIRE", "Unknown"),
-                    "fill": feature["properties"].get("fill", "#000000"),
+                    "fill": feature["properties"].get("fill","#000000"),
                     "stroke": feature["properties"].get("stroke", "#FFFFFF"),
                 }
 
